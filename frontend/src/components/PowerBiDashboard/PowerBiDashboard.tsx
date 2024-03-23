1a30a68cf96cb6ee75715eb1078c876b28c01e9e
Hugo Levet <hugolevet.pro@gmail.com>
2024-03-23 22:33:51 +0100

    Add color variables and styles for GoogleTrends and PowerBiDashboard components

--- frontend/src/components/PowerBiDashboard/PowerBiDashboard.tsx
+++ frontend/src/components/PowerBiDashboard/PowerBiDashboard.tsx
@@ -4,7 +4,7 @@ function PowerBiDashboard() {
 
     return (
         <section className={style.container}>
-            <iframe title="Store Sales" src="https://app.powerbi.com/view?r=eyJrIjoiMjhhYTJiNGMtZGQyNS00NWZhLWEzZTktYjgyNzIzYTc3MGZjIiwidCI6ImI5M2Y0MDhjLWJmYjEtNGVmNC1iZDJkLTFiMGRkNmQwOGQyNSJ9" allowFullScreen={true}></iframe>
+            <iframe title="Store Sales" src="https://app.powerbi.com/view?r=eyJrIjoiMDA4NmQ1Y2YtNTM5OC00MzMwLTg5ZjUtN2U3YTA1MzA4OWYwIiwidCI6ImI5M2Y0MDhjLWJmYjEtNGVmNC1iZDJkLTFiMGRkNmQwOGQyNSJ9" allowFullScreen={true}></iframe>
         </section>
     )
 }
