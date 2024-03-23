bd870b04089af8b4ef4908803f5003d59b2d631f
Hugo Levet <hugolevet.pro@gmail.com>
2024-03-23 12:53:13 +0100

    Update PowerBiDashboard iframe source

--- frontend/src/components/PowerBiDashboard/PowerBiDashboard.tsx
+++ frontend/src/components/PowerBiDashboard/PowerBiDashboard.tsx
@@ -4,7 +4,7 @@ function PowerBiDashboard() {
 
     return (
         <section className={style.container}>
-            <iframe title="Store Sales" src="https://app.powerbi.com/view?r=eyJrIjoiMGZhZWE0NjgtZTQ1Ni00ZDMzLWJlNjktOWExMDIwZmNkMWMxIiwidCI6ImI5M2Y0MDhjLWJmYjEtNGVmNC1iZDJkLTFiMGRkNmQwOGQyNSJ9" allowFullScreen={true}></iframe>
+            <iframe title="Store Sales" src="https://app.powerbi.com/view?r=eyJrIjoiMjhhYTJiNGMtZGQyNS00NWZhLWEzZTktYjgyNzIzYTc3MGZjIiwidCI6ImI5M2Y0MDhjLWJmYjEtNGVmNC1iZDJkLTFiMGRkNmQwOGQyNSJ9" allowFullScreen={true}></iframe>
         </section>
     )
 }
