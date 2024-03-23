7f90c3fe570c8af35281ae756ad6e1d54812bc54
Hugo Levet <hugolevet.pro@gmail.com>
2024-03-23 13:05:51 +0100

    Add constants for colors in frontend

---
+++ frontend/src/const.ts
@@ -0,0 +1,5 @@
+export const BACKGROUND_COLOR = "#002626";
+export const SECONDARY_BACKGROUND_COLOR = "#002626";
+export const CURVE_COLOR = "#002626";
+export const SECOND_CURVE_COLOR = "#002626";
+export const TEXT_COLOR = "#EFE7DA";
