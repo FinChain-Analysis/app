8cdf1ccfe1c532cadd85260ad6e025d9b7e3010e
Hugo Levet <hugolevet.pro@gmail.com>
2024-03-23 13:55:01 +0100

    Swap curve colors in const.ts

--- frontend/src/const.ts
+++ frontend/src/const.ts
@@ -1,5 +1,5 @@
 export const BACKGROUND_COLOR = "#002626";
 export const SECONDARY_BACKGROUND_COLOR = "#0e4749";
-export const CURVE_COLOR = "#9f9fed";
-export const SECOND_CURVE_COLOR = "#f61067";
+export const CURVE_COLOR = "#f61067";
+export const SECOND_CURVE_COLOR = "#9f9fed";
 export const TEXT_COLOR = "#efe7da";
